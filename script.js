a08d671b59b28afee4a043a1b4418edfd52c032c
diegogarciaxd2020h-wq <diegogarcia.xd2020h@gmail.com>
2026-07-25 21:00:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 script.js
+const button = document.querySelector(".generate");
+
+const response = document.querySelector(".response p");
+
+
+button.addEventListener("click",()=>{
+
+
+let frases=[
+
+"Podrías decir: Me parece que hablar contigo siempre mejora mi día 😏",
+
+"Prueba con algo divertido: Creo que necesito una explicación... ¿cómo haces para caer tan bien?",
+
+"Una opción romántica: Me gusta la energía que transmites, es difícil no querer conocerte más."
+
+];
+
+
+let random =
+frases[Math.floor(Math.random()*frases.length)];
+
+
+response.innerHTML=random;
+
+
+});
